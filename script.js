c730f8383ba76b6d6c85e0fbce6395b35b770e6b
YanTano <101397014+YanTano@users.noreply.github.com>
2025-04-30 16:45:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // Firebase configuration object
 const firebaseConfig = {
   apiKey: "AIzaSyAS7gU_hxP7wg-o9U-yuOG7DBvSGX-nmho",
@@ -10,8 +12,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = firebase.firestore(app);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Get references to DOM elements
 const workoutForm = document.getElementById('workout-form');
